b17315241e46080d91a4202286a8294e5f7b8ca8
gccosta13 <gccosta2005@gmail.com>
2025-03-07 00:44:34 +0000

    right

---
+++ RightSize/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace RightSize
+{
+    public class Program
+    {
+        private static void Main(string[] args)
+        {
+            foreach (string arg in args)
+            {
+                if (arg.Length >= 8)
+                {
+                    Console.WriteLine("[EARLY STOP]");
+                    return; // Termina o programa imediatamente
+                }
+
+                if (arg.Length > 3)
+                {
+                    Console.WriteLine(arg);
+                }
+            }
+        }
+    }
+}
+
